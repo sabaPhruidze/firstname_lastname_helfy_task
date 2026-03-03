d4a39bbd4dbf16e9195ea2771f24fe4a19ba3ddf
Saba <sabaphruidze01@gmail.com>
2026-03-03 17:41:47 +0400

    feat(taskForm):added title and description and pending adds automatically, after what I wrote, next will be adding priority as well and correcting taskform fully

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import { useEffect, useState } from "react";
-import { getTasks } from "./services/api";
+import { getTasks, postTask } from "./services/api";
 import TaskFilter from "./components/TaskFilter";
+import TaskForm from "./components/TaskForm";
 function App() {
   const [tasks, setTasks] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState();
   const [filter, setFilter] = useState("all");
+  const [saving, setSaving] = useState(false);
   useEffect(() => {
     const fetchTasks = async () => {
       try {
@@ -27,12 +29,25 @@ function App() {
     if (Object.is(filter, "pending")) return !task.completed;
     return true; // for returning all
   });
+  const createTasks = async (payload) => {
+    try {
+      setSaving(true);
+      setError("");
+      const create = await postTask(payload);
+      setTasks((prev) => [create, ...prev]); // for the order
+    } catch (e) {
+      setError(e.message || "error can not create task");
+    } finally {
+      setSaving(false);
+    }
+  };
   if (loading) return <div>tasks will be presented soon...</div>;
   if (error) return <div>{error}</div>;
   return (
     <div>
       <h1>Task manager</h1>
       <TaskFilter value={filter} onChange={setFilter} />
+      <TaskForm onSubmit={createTasks} loading={saving} />
       <p>filter: {filter}</p>
       {tasks.length === 0 ? (
         <p>No tasks added yet</p>
--- frontend/src/components/TaskForm.jsx
+++ frontend/src/components/TaskForm.jsx
@@ -1,7 +1,40 @@
-import React from "react";
+import { useState } from "react";
 
-const TaskForm = () => {
-  return <div>TaskForm</div>;
+const possiblePriorities = ["low", "medium", "high"];
+const TaskForm = ({ onSubmit, loading }) => {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+
+  const [error, setError] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onSubmit({
+      title: title,
+      description: description,
+    });
+    console.log(title, description);
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      <h3>Add Task</h3>
+      {error ? <p>{error}</p> : ""}
+      <input
+        type="text"
+        placeholder="title"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+      />
+      <textarea
+        name="description"
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
+      <button type="submit" disabled={loading}>
+        {loading ? "Please wait..." : "Add Task"}
+      </button>
+    </form>
+  );
 };
 
 export default TaskForm;
