f20315c4ed016ac657b686ab3f5c2a1454a2216f
Saba <sabaphruidze01@gmail.com>
2026-03-03 12:22:55 +0400

    feat(server):added post method

--- README.md
+++ README.md
@@ -11,7 +11,16 @@
 6. added cors and expres.json middleware
 7. added GET method on /api/tasks route .
 8. created an array tasks instead of database and added one case to test . it displays succesfully here: http://localhost:4000/api/tasks
-9.
+9. toISOString() used in order to make date more understandable than Date.now() example:"2026-03-03T07:21:46.886Z"
+10. added POST method on /api/tasks
+    {
+    "title": "testing post method",
+    "description": "write title description and priotity to check if it works or not using thunder clients",
+    "priority": "mediumfew"
+    }
+    wrote priority like this and it added low but when I wrote medium it added correctly
+11. check of post and get method was done using thundertask
+12.
 
 ## Frontend Setup
 
--- backend/server.js
+++ backend/server.js
@@ -9,22 +9,48 @@ app.use(express.json());
 app.get("/check", (_req, res) => {
   res.status(200).send("server runs");
 });
-// tasks GET & POST [START]
-let tasks = [
-  {
-    id: 1,
-    title: "dance",
-    description: "1 hour dance results",
-    completed: true,
-    createdAt: new Date().toISOString(),
-    priority: "low",
-  },
-];
+// tasks GET [START]
+let tasks = [];
 
 app.get("/api/tasks", (_req, res) => {
   res.status(200).json(tasks);
 });
-// tasks GET & POST [END]
+// tasks GET [END]
+// tasks POST [START]
+const possiblePriorities = ["low", "medium", "high"];
+let newId = 1;
+app.post("/api/tasks", (req, res) => {
+  const { title, description, priority } = req.body;
+  const checkTitle = typeof title === "string" ? title.trim() : "";
+  if (!checkTitle) {
+    return res
+      .status(400)
+      .json({ message: "title is emtpy. please write the title" });
+  }
+  const checkDescriptionm =
+    typeof description === "string" ? description.trim() : "";
+  if (!checkDescriptionm) {
+    return res
+      .status(400)
+      .json({ message: "description is emtpy. please write the description" });
+  }
+  const checkPriority = possiblePriorities.includes(priority)
+    ? priority
+    : "low";
+
+  const newTask = {
+    id: newId,
+    title: checkTitle,
+    description: checkDescriptionm,
+    completed: false,
+    createdAt: new Date().toISOString(),
+    priority: checkPriority,
+  };
+  tasks.unshift(newTask);
+  newId += 1;
+  return res.status(201).json(newTask);
+});
+// tasks POST [END]
 app.listen(PORT, () => {
   console.log(
     `server runs succesfully on this link: http://localhost:${PORT}/api/tasks`,
